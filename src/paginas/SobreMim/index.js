a988e94049c84eb0da0d8d4787527165b1e18946
Renata <renata0cristiane@gmail.com>
2024-05-30 00:44:04 -0300

    modificando a foto

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -23,21 +23,31 @@ export default function SobreMim() {
             <p className={styles.paragrafo}>
                 Oi, tudo bem? Eu sou Desenvolvedora e estou feliz de te ver por aqui.
             </p>
+           
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
-            </p>
-            <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
-            </p>
-            <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
-            </p>
-            <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
+               Sou um profissional em desenvolvimento na área de tecnologia, com forte compromisso com o aprendizado contínuo e o crescimento pessoal. O meu percurso começou na área de gestão de riscos, onde desenvolvi sólidas competências em análise, prevenção e mitigação de riscos. Essa experiência me proporcionou uma base sólida de responsabilidade e atenção aos detalhes.
 
             </p>
             <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
+                Recentemente, tenho me dedicado ao desenvolvimento de software, participando ativamente de projetos front-end e back-end. Trabalhei com tecnologias como JavaScript, TypeScript, React, Three.js, Tailwind CSS, HTML e CSS para criar interfaces de usuário dinâmicas e intuitivas. No back-end, utilizei Java Spring Boot, Python, Node.js, FastAPI e ferramentas de automação como Robot Framework e Selenium para desenvolver soluções robustas e eficientes. Também usei Pandas para análise de dados.
+            </p>
+            <p className={styles.paragrafo}>
+               Atualmente, estou aprimorando minhas habilidades em diversas linguagens e ferramentas, incluindo MySQL e Arduino. Minhas experiências em projetos pessoais e em equipe me permitiram aplicar meus conhecimentos teóricos na prática, adquirindo experiência em todas as etapas do desenvolvimento de software.
+            </p>
+            <p className={styles.paragrafo}>
+                Além do trabalho técnico, possuo experiência em vendas, onde desenvolvi habilidades em negociação e atendimento ao cliente, além de conhecimentos em gestão empresarial e empreendedorismo. Minha capacidade de adaptação e aprendizado rápido me permite navegar em diferentes ambientes e situações com eficiência.
+
+            </p>
+             <p className={styles.paragrafo}>
+                Sou uma pessoa que se adapta facilmente a diversos ambientes e situações, proativa, buscando sempre colaborar e aprender com meus colegas. Valorizo o trabalho em equipe e acredito na importância da comunicação eficaz e da empatia no local de trabalho. Meu envolvimento em minicamps, bootcamps, hackathons e programas de treinamento em tecnologia demonstra meu compromisso com o desenvolvimento contínuo.
+
+            </p>
+             <p className={styles.paragrafo}>
+                Além disso, dedico parte do meu tempo ao voluntariado, acreditando na importância de contribuir positivamente para a sociedade. Esta experiência me ensinou a trabalhar bem em grupo, desenvolver habilidades de liderança e manter uma atitude de paciência e perseverança em todas as minhas funções.
+            </p>
+             <p className={styles.paragrafo}>
+                Estou sempre pronto para novos desafios e oportunidades que me permitam crescer e contribuir significativamente nos projetos e equipes com quem trabalho.
+
             </p>
         </PostModelo>
     )
